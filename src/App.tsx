74160ed670e447469fd1c8d9f06effaf4118e1a9
Miguel Lewandowski <miguelstrapazonl@gmail.com>
2026-01-14 20:55:40 -0300

    refactor: refatora App para incluir novos componentes e paginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import Home from "./pages/home/Home"
+import './App.css'
+import Login from './pages/login/Login'
 
 function App() {
+
   return (
     <>
-      <Home/>
+      <Login />
     </>
   )
 }
