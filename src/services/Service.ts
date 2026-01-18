1ec62e8a19742c76733f92d384a95e0c3ca61d14
Miguel Lewandowski <miguelstrapazonl@gmail.com>
2026-01-18 15:58:04 -0300

    feat: adiciona função de cadastro com dados da api

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://blogpessoal-8nik.onrender.com/'
+    baseURL: 'https://blogpessoal-8nik.onrender.com'
 })
 
 export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
