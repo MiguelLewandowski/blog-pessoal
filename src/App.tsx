f48225f2bce3a7d9d9ff2962c0d833f23cfbc702
Miguel Lewandowski <miguelstrapazonl@gmail.com>
2026-01-19 23:08:16 -0300

    feat: cria card tema e lista de temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
-
+import { AuthProvider } from './contexts/AuthContext'
 import Cadastro from './pages/cadastro/Cadastro'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
-import { AuthProvider } from './contexts/AuthContext'
+import ListaTemas from './components/tema/listaTemas/ListaTemas'
 
 function App() {
 	return (
 		<>
 			<AuthProvider>
-				<ToastContainer />
 				<BrowserRouter>
 					<Navbar />
 					<div className="min-h-[80vh]">
@@ -21,6 +18,7 @@ function App() {
 							<Route path="/" element={<Login />} />
 							<Route path="/home" element={<Home />} />
 							<Route path="/cadastro"	element={<Cadastro />}/>
+							<Route path="/temas" element={<ListaTemas />} />
 						</Routes>
 					</div>
 					<Footer />
