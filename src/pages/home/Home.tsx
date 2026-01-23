eb7cd88a75cc55379cfc38363aef84cd7ef9f1d7
Miguel Lewandowski <miguelstrapazonl@gmail.com>
2026-01-22 22:30:57 -0300

    feat: adiciona logica de cardPostagem e listaPostagem

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,22 @@
+import ListaPostagens from "../../components/postagem/listaPostagem/ListaPostagem";
+
+
 function Home() {
     return (
-        <div className="max-w-3xl mx-auto text-center py-20 space-y-10">
-            <h2 className="text-5xl font-bold tracking-tight">Seja bem-vindo!</h2>
-            <div className="rounded-lg border border-gray-800 bg-gray-900 p-8 text-left space-y-3">
-                <h3 className="text-2xl font-semibold">Minha postagem</h3>
-                <p className="text-gray-300">
-                    Gosto de basquete, xadrez, futebol, músicas e viagens. 
-                    Aqui compartilho um pouco dos interesses que me inspiram no dia a dia.
-                </p>
+        <>
+            <div className="max-w-3xl mx-auto text-center py-20 space-y-10">
+                <h2 className="text-5xl font-bold tracking-tight">Seja bem-vindo!</h2>
+                <div className="rounded-lg border border-gray-800 bg-gray-900 p-8 text-left space-y-3">
+                    <h3 className="text-2xl font-semibold">Minha postagem</h3>
+                    <p className="text-gray-300">
+                        Gosto de basquete, xadrez, futebol, músicas e viagens.
+                        Aqui compartilho um pouco dos interesses que me inspiram no dia a dia.
+                    </p>
+                </div>
             </div>
-        </div>
+            <ListaPostagens />
+        </>
+
     );
 }
 
